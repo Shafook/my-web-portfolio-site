d56712bfde6059fa165124059e40bfde0d589104
Shafook <shafook@hotmail.com>
2022-04-11 18:58:46 -0400

    small changes

--- src/app/js/data/aboutme.js
+++ src/app/js/data/aboutme.js
@@ -16,10 +16,10 @@ export default [
   },
 
   {
-    title: 'Lorem ipsum dolor sit amet.',
+    title: 'Communication',
     image:
       'https://images.unsplash.com/photo-1517048676732-d65bc937f952?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo similique, inventore error pariatur qui, aut aperiam in quasi voluptate architecto quos reprehenderit voluptates animi odio aliquid odit totam recusandae quaerat?',
+      'I interacting with others in sharing ideas and learning from each others. I am a team player who enjoys working with others.',
   },
 ];
